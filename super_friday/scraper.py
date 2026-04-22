e2626c9c49a2939934deccdd26219dd3911858ce
kay827171-dev <kay827171@gmail.com>
2026-04-22 10:49:39 +0200

    edit

--- super_friday/scraper.py
+++ super_friday/scraper.py
@@ -213,8 +213,8 @@ class SuperFridayScraper:
             old_price_el = await detail_page.query_selector('.old-price .price')
             product_data['old_price'] = (await old_price_el.inner_text()).strip() if old_price_el else None
             
-            # Special price
-            special_price_el = await detail_page.query_selector('.special-price .price')
+            # Special price (handles both .special-price and .normal-price display cases)
+            special_price_el = await detail_page.query_selector('.special-price .price, .normal-price .price')
             product_data['special_price'] = (await special_price_el.inner_text()).strip() if special_price_el else None
             
             # Description
